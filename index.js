e9f3a47adc9879b65ad5b4609c9d8997fc557207
THE PROGRAMMER <142110420+Itxxwasi@users.noreply.github.com>
2024-04-30 13:02:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/**const bot = require(__dirname + '/lib/amd')
+const bot = require(__dirname + '/lib/amd')
 const { VERSION } = require(__dirname + '/config')
 
 const start = async () => {
@@ -13,4 +13,4 @@ const start = async () => {
     start();
   }
 }
-start();**/
+start();
