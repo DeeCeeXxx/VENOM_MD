d76ba017b46564a6f6bc1e35aaff3f5795057cb6
Hammy254 <164391481+Hammy254@users.noreply.github.com>
2024-06-07 20:52:49 +0300

    𝐕𝐄𝐍𝐎𝐌_𝐌𝐃

--- plugins/group.js
+++ plugins/group.js
@@ -92,7 +92,7 @@ const {
        var _0x2fe2c7 = "" + _0x2608ab + _0x3a49e9;
        var _0x539d8f = {
          externalAdReply: {
-           title: "𝐕𝐄𝐍𝐎𝐌_𝐌𝐃",
+           title: "𝐕𝐄𝐍𝐎𝐌 𝐌𝐃",
            body: "" + _0x37b490,
            renderLargerThumbnail: true,
            thumbnail: log0,
